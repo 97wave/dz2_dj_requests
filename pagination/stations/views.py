592633f0fe691c6b34274155d9a82df66db31518
97wave <e97.wav@gmail.com>
2021-08-29 11:49:26 +0300

    First

---
+++ pagination/stations/views.py
@@ -0,0 +1,35 @@
+import csv
+from django.urls import reverse
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+def bus_stations(request):
+    csv_path = str(settings.BUS_STATION_CSV)
+    DATA = []
+    with open(csv_path, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            bus_station = {
+                'Name' : row['Name'], 
+                'Street': row['Street'], 
+                'District' : row['District']
+            }
+            DATA.append(bus_station)
+
+    page_number = int(request.GET.get('page', 1))
+    element_per_page = 10
+    
+    paginator = Paginator(DATA, element_per_page)
+    page = paginator.get_page(page_number)
+    data = page.object_list
+
+    context = {
+        'bus_stations': data,
+        'page': page
+    }
+    return render(request, 'stations/index.html', context)
---
+++ recipes/calculator/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, кг': 0.3,
+        'сыр, кг': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    }
+}
+
+def calculate_view(request, dish_name):
+    person = int(request.GET.get('servings', '1'))
+    context = {
+        'recipe' : {}
+        }
+
+    for ingridient, qty in DATA[dish_name].items():
+        context['recipe'][ingridient] = person * DATA[dish_name][ingridient]
+    
+    return render(request, 'calculator/index.html', context)
+    
